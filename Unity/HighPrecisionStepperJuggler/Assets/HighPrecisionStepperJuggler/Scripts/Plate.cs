578d4673e6864e3c350b434c84d1c2fd7856d490
T-Kuhn <tkuhn.mpu@gmail.com>
2019-10-06 16:38:11 +0900

    added plate script and updated .gitignore

---
+++ Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/Plate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace HighPrecisionStepperJuggler
+{
+    [ExecuteInEditMode]
+    public class Plate : MonoBehaviour
+    {
+        [SerializeField] private Transform _motor1Joint2Tip;
+        [SerializeField] private Transform _motor2Joint2Tip;
+
+        void Update()
+        {
+            // set height
+            var meanHeight = (_motor1Joint2Tip.position.y + _motor2Joint2Tip.position.y) / 2f;
+            transform.position = Vector3.up * meanHeight;
+
+            // set tilt
+
+        }
+    }
+}
