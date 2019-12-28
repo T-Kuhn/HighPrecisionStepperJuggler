bddf4ad30131b2b1af6279054e51bbae53d87d65
T-Kuhn <tkuhn.mpu@gmail.com>
2019-12-28 18:02:57 +0900

    moving model without plate movements and no Y-Offset correction

--- Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/BasicJoint.cs
+++ Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/BasicJoint.cs
@@ -4,14 +4,35 @@ namespace HighPrecisionStepperJuggler
 {
     public class BasicJoint : MonoBehaviour
     {
+        private enum RotationalAxis {X, Y, Z}
+        [SerializeField] private RotationalAxis _rotAxis;
+        
         [SerializeField] private Transform _tip = null;
         [SerializeField] private Transform _attachToTip = null;
 
         public float JointRotation;
 
+        private Quaternion _startRotation;
+
+        private void Awake()
+        {
+            _startRotation = transform.localRotation;
+        }
+
         public void UpdatePositionAndRotation()
         {
-            transform.localRotation = Quaternion.Euler(0f, 0f, JointRotation * Mathf.Rad2Deg);
+            switch (_rotAxis)
+            {
+                case RotationalAxis.X:
+                    transform.localRotation = Quaternion.Euler(-JointRotation * Mathf.Rad2Deg, 0f, 0f) * _startRotation;
+                    break;
+                case RotationalAxis.Y:
+                    transform.localRotation = Quaternion.Euler(0f, -JointRotation * Mathf.Rad2Deg, 0f) * _startRotation;
+                    break;
+                case RotationalAxis.Z:
+                    transform.localRotation = Quaternion.Euler( 0f, 0f, -JointRotation * Mathf.Rad2Deg) * _startRotation;
+                    break;
+            }
             
             if (_attachToTip != null)
             {
--- Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/Constants.cs
+++ Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/Constants.cs
@@ -6,8 +6,8 @@
         public static readonly float PlateWidth = 0.08f;
         
         // Length of Links
-        public static readonly float L1 = 0.04f;
-        public static readonly float L2 = 0.05f;
+        public static readonly float L1 = 0.089f;
+        public static readonly float L2 = 0.080f;
         
         // Scale of parent GameObject
         public static float RootObjectScale = 30f;
