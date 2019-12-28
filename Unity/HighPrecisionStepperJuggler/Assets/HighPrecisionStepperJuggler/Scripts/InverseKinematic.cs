d1bd3b6354a4116010657c422f8c5029a5297211
T-Kuhn <tkuhn.mpu@gmail.com>
2019-12-29 00:45:28 +0900

    attempt at implementing the q-offset (not working)

--- Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/InverseKinematic.cs
+++ Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/InverseKinematic.cs
@@ -26,17 +26,42 @@ namespace HighPrecisionStepperJuggler
         /// <returns>theta2 (rotation of joint2)</returns>
         public static float CalculateJoint2RotationFromJoint1Rotation(float theta1)
         {
-            return (float)(Mathf.PI - Math.Acos(Mathf.Cos(theta1) * c.L1 / c.L2));
+            var q = -0.04f;
+            
+            return (float)(Mathf.PI - Math.Acos((Mathf.Cos(theta1) * c.L1 + q) / c.L2));
         }
-        
+
         /// <summary>
-        /// Get joint1 rotation with a given theta1 rotation
+        /// Get joint1 rotation with a given targetHeight
         /// </summary>
         /// <param name="targetY">The given target height (tip of link2)</param>
         /// <returns>theta1 (rotation of joint1)</returns>
         public static float CalculateJoint1RotationFromTargetY(float targetY)
         {
-            return Mathf.Asin((targetY * targetY + c.L1 * c.L1 - c.L2 * c.L2) / (2f * c.L1 * targetY));
+            //return Mathf.Asin((targetY * targetY + c.L1 * c.L1 - c.L2 * c.L2) / (2f * c.L1 * targetY));
+
+            var q = -0.04f;
+            
+            var a1 = 1f / (2f * (c.L1 * c.L1 * q * q + c.L1 * c.L1 * targetY * targetY));
+            var a2 = c.L2 * c.L2 * c.L1 * q;
+            var a3 = Mathf.Sqrt(-Mathf.Pow(c.L2, 4f) * c.L1 * c.L1 * targetY * targetY
+                                + 2f * c.L2 * c.L2 * Mathf.Pow(c.L1, 4f) * targetY * targetY
+                                + 2f * c.L2 * c.L2 * c.L1 * c.L1 * q * q * targetY * targetY
+                                + 2f * c.L1 * c.L1 * c.L2 * c.L2 * Mathf.Pow(targetY, 4f)
+                                + Mathf.Pow(c.L1, 6f) * -(targetY * targetY)
+                                + 2f * Mathf.Pow(c.L1, 4f) * q * q * targetY * targetY
+                                + 2f * Mathf.Pow(c.L1, 4f) * Mathf.Pow(targetY, 4f)
+                                - c.L1 * c.L1 * Mathf.Pow(q, 4f) * targetY * targetY
+                                - 2f * c.L1 * c.L1 * q * q * Mathf.Pow(targetY, 4f)
+                                - c.L1 * c.L1 * Mathf.Pow(targetY, 6f));
+            var a4 = Mathf.Pow(c.L1, 3f) * q + c.L1 * Mathf.Pow(q, 3f) + c.L1 * q * targetY*targetY;
+            
+            // NOTE: k is c.L2
+            //cos^(-1)((-k^2 l q +/- sqrt(-k^4 l^2 y^2 + 2 k^2 l^4 y^2 + 2 k^2 l^2 q^2 y^2 + 2 k^2 l^2 y^4
+            //    + l^6 (-y^2) + 2 l^4 q^2 y^2 + 2 l^4 y^4 - l^2 q^4 y^2 - 2 l^2 q^2 y^4 - l^2 y^6)
+            //    + l^3 q + l q^3 + l q y^2)/(2 (l^2 q^2 + l^2 y^2)))
+            return Mathf.Acos(a1 * (-a2 + a3 + a4));
+            //return Mathf.Acos(a1 * (-a2 - a3 + a4));
         }
     }
 }
