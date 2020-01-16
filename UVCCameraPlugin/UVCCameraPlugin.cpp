d88702c4f8aa4e5d95cea8bdbb0d5540d401b507
T-Kuhn <tkuhn.mpu@gmail.com>
2020-01-16 23:55:25 +0900

    testing openCVs hough transform circle detection

--- UVCCameraPlugin/UVCCameraPlugin.cpp
+++ UVCCameraPlugin/UVCCameraPlugin.cpp
@@ -1,6 +1,13 @@
+
 #include "UVCCameraPlugin.h"
 #include <opencv2/opencv.hpp>
 #include <cstdio>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
 
 void* getCamera()
 {
@@ -38,9 +45,33 @@ void getCameraTexture(void* camera, unsigned char* data)
     cv::Mat img;
     *cap >> img;
 
+    // START
+    Mat src = img;
+
+    Mat gray;
+    cvtColor(src, gray, COLOR_BGR2GRAY);
+    medianBlur(gray, gray, 5);
+    vector<Vec3f> circles;
+    HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
+        gray.rows / 4,  // change this value to detect circles with different distances to each other
+        100, 30, 5, 120 // change the last two parameters
+   // (min_radius & max_radius) to detect larger circles
+    );
+    for (size_t i = 0; i < circles.size(); i++)
+    {
+        Vec3i c = circles[i];
+        Point center = Point(c[0], c[1]);
+        // circle center
+        circle(src, center, 1, Scalar(0, 100, 100), 3, LINE_AA);
+        // circle outline
+        int radius = c[2];
+        circle(src, center, radius, Scalar(255, 0, 255), 3, LINE_AA);
+    }
+    // END.
+
     // RGB --> ARGB •ÏŠ·
     cv::Mat argb_img;
-    cv::cvtColor(img, argb_img, 2);
+    cv::cvtColor(src, argb_img, 2);
     std::vector<cv::Mat> bgra;
     cv::split(argb_img, bgra);
     std::swap(bgra[0], bgra[3]);
