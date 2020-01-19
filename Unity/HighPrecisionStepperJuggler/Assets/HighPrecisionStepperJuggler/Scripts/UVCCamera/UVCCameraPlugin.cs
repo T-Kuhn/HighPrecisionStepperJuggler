634c3fb3e5512094c0c4acfec93da4499de0a9d8
T-Kuhn <tkuhn.mpu@gmail.com>
2020-01-19 13:15:53 +0900

    optimized way of setting/getting camera properties

--- Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/UVCCamera/UVCCameraPlugin.cs
+++ Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/UVCCamera/UVCCameraPlugin.cs
@@ -13,7 +13,7 @@ namespace HighPrecisionStepperJuggler
 
         [DllImport("UVCCameraPlugin")]
         private static extern double getCameraProperty(IntPtr camera, int propertyId);
-        
+
         [DllImport("UVCCameraPlugin")]
         private static extern double setCameraProperty(IntPtr camera, int propertyId, double value);
 
@@ -26,7 +26,8 @@ namespace HighPrecisionStepperJuggler
         private readonly int Width = 640;
         private readonly int Height = 480;
         private readonly int Exposure = -7;
-        private readonly int Gain = 15;
+        private readonly int Gain = 2;
+        private readonly int Saturation = 22;
 
         private IntPtr _camera;
         private Texture2D _texture;
@@ -35,6 +36,7 @@ namespace HighPrecisionStepperJuggler
         private IntPtr _pixelsPtr;
 
         [SerializeField] private RenderTexture _renderTexture = null;
+        [SerializeField] private CameraProperties _properties;
 
         void Start()
         {
@@ -44,30 +46,53 @@ namespace HighPrecisionStepperJuggler
             setCameraProperty(_camera, (int) vcp.CAP_PROP_FRAME_WIDTH, Height);
             setCameraProperty(_camera, (int) vcp.CAP_PROP_EXPOSURE, Exposure);
             setCameraProperty(_camera, (int) vcp.CAP_PROP_GAIN, Gain);
-            
+            setCameraProperty(_camera, (int) vcp.CAP_PROP_SATURATION, Saturation);
+
             _texture = new Texture2D(Width, Height, TextureFormat.ARGB32, false);
             _pixels = _texture.GetPixels32();
             _pixelsHandle = GCHandle.Alloc(_pixels, GCHandleType.Pinned);
             _pixelsPtr = _pixelsHandle.AddrOfPinnedObject();
         }
 
-        public double GetCameraProperty(vcp property)
+        private double GetCameraProperty(vcp property)
         {
-            return getCameraProperty(_camera, (int)property);
+            return getCameraProperty(_camera, (int) property);
         }
 
-        public void SetCameraProperty(vcp property, double value)
+        private void SetCameraProperty(vcp property, double value)
         {
             setCameraProperty(_camera, (int) property, value);
+        }
+
+        public void GetCameraProperties()
+        {
+            _properties.Width = GetCameraProperty(vcp.CAP_PROP_FRAME_WIDTH);
+            _properties.Height = GetCameraProperty(vcp.CAP_PROP_FRAME_HEIGHT);
+            _properties.FPS = GetCameraProperty(vcp.CAP_PROP_FPS);
+            
+            _properties.Exposure = GetCameraProperty(vcp.CAP_PROP_EXPOSURE);
+            _properties.Gain = GetCameraProperty(vcp.CAP_PROP_GAIN);
+            _properties.Contrast = GetCameraProperty(vcp.CAP_PROP_CONTRAST);
+            _properties.ISO = GetCameraProperty(vcp.CAP_PROP_ISO_SPEED);
+            _properties.Saturation = GetCameraProperty(vcp.CAP_PROP_SATURATION);
+        }
+
+        public void SetCameraProperties()
+        {
+            SetCameraProperty(vcp.CAP_PROP_EXPOSURE, _properties.Exposure);
+            SetCameraProperty(vcp.CAP_PROP_GAIN, _properties.Gain);
+            SetCameraProperty(vcp.CAP_PROP_CONTRAST, _properties.Contrast);
+            SetCameraProperty(vcp.CAP_PROP_ISO_SPEED, _properties.ISO);
+            SetCameraProperty(vcp.CAP_PROP_SATURATION, _properties.Saturation);
         }
 
         void Update()
         {
             getCameraTexture(_camera, _pixelsPtr);
-            
+
             _texture.SetPixels32(_pixels);
             _texture.Apply();
-            
+
             Graphics.Blit(_texture, _renderTexture);
         }
 
@@ -76,5 +101,18 @@ namespace HighPrecisionStepperJuggler
             _pixelsHandle.Free();
             releaseCamera(_camera);
         }
+    }
+
+    [Serializable]
+    struct CameraProperties
+    {
+        public double Width;
+        public double Height;
+        public double FPS;
+        public double Exposure;
+        public double Gain;
+        public double Contrast;
+        public double ISO;
+        public double Saturation;
     }
 }
--- Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/UVCCamera/UVCCameraPluginEditor.cs
+++ Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/UVCCamera/UVCCameraPluginEditor.cs
@@ -13,23 +13,14 @@ namespace HighPrecisionStepperJuggler
             DrawDefaultInspector();
 
             var script = (UVCCameraPlugin) target;
-            if (GUILayout.Button("SetProperties", GUILayout.Width(200)))
-            {
-                script.SetCameraProperty(vcp.CAP_PROP_EXPOSURE, -7);
-                script.SetCameraProperty(vcp.CAP_PROP_GAIN, 10);
-            }
-
             if (GUILayout.Button("GetProperties", GUILayout.Width(200)))
             {
-                var width = script.GetCameraProperty(vcp.CAP_PROP_FRAME_WIDTH);
-                var height = script.GetCameraProperty(vcp.CAP_PROP_FRAME_HEIGHT);
-                var exposure = script.GetCameraProperty(vcp.CAP_PROP_EXPOSURE);
-                var gain = script.GetCameraProperty(vcp.CAP_PROP_GAIN);
+                script.GetCameraProperties();
+            }
 
-                Debug.Log("width: " + width);
-                Debug.Log("height: " + height);
-                Debug.Log("exposure: " + exposure);
-                Debug.Log("gain: " + gain);
+            if (GUILayout.Button("SetProperties", GUILayout.Width(200)))
+            {
+                script.SetCameraProperties();
             }
         }
     }
