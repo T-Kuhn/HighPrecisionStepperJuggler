8c7b4850d8624fbe5f122d18b5c294548056d855
T-Kuhn <tkuhn.mpu@gmail.com>
2020-04-26 14:45:08 +0900

    made tilt visualization ignore tilt offset

--- Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/TiltMeterPresenter.cs
+++ Unity/HighPrecisionStepperJuggler/Assets/HighPrecisionStepperJuggler/Scripts/TiltMeterPresenter.cs
@@ -9,13 +9,30 @@ namespace HighPrecisionStepperJuggler
         [SerializeField] private TiltMeterView _xTiltMeter;
         [SerializeField] private TiltMeterView _yTiltMeter;
 
+        private float _xLineLength;
+        private float _yLineLength;
+
+        private Vector2 _offset = Vector2.zero;
+
         private void Start()
         {
             _tiltingPlate.OnTiltUpdate += (xTilt, yTilt) =>
             {
-                _xTiltMeter.LineLength = xTilt * 20f;
-                _yTiltMeter.LineLength = yTilt * 20f;
+                _xLineLength = xTilt * 20f;
+                _xTiltMeter.LineLength = _xLineLength - _offset.x;
+
+                _yLineLength = yTilt * 20f;
+                _yTiltMeter.LineLength = _yLineLength - _offset.y;
             };
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _offset.x = _xLineLength;
+                _offset.y = _yLineLength;
+            }
         }
     }
 }
